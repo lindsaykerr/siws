833fc8e7a57bb447af3fa53cb893be86e181881d
lindsay <wrenzenzen@gmail.com>
2025-07-11 14:36:17 +0000

    Vite Build Settings update

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,43 @@ import { defineConfig } from 'vite'
 import myconfig from './project.config';
 
 
-export default defineConfig({
+export default defineConfig(({command, mode}) => {
 
-    server: {
-        allowedHosts: myconfig.server.allowedHosts,
-   
-        hmr: {
-            host: myconfig.server.hmr.host,
-            port: myconfig.server.hmr.port,
-            protocol: myconfig.server.hmr.protocol,
-            clientPort: myconfig.server.hmr.clientPort,
-        },
-      },
-      base: myconfig.base,
+    if (command === 'serve') {
+        return {
+            server: {
+                allowedHosts: myconfig.dev.server.allowedHosts,
+        
+                hmr: {
+                    host: myconfig.dev.server.hmr.host,
+                    port: myconfig.dev.server.hmr.port,
+                    protocol: myconfig.dev.server.hmr.protocol,
+                    clientPort: myconfig.dev.server.hmr.clientPort,
+                },
+            },
+            base: myconfig.dev.base,
+        };
+    }
+    else {
+        return{
+            base: myconfig.build.base || '/',
+            outDir: 'dist',
+            build: {
+                minify: 'esbuild',
+                sourcemap: true,
+                rollupOptions: {
+                    output: {
+                        manualChunks: {
+                            'three': ['three'],
+                            //'three-examples': ['three/examples/jsm/'],
+                            'jsqr': ['jsqr'],
+                        },
+                    },
+                },
+            },
+        }
+    }
+
 
     
 });
